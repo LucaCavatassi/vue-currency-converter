94b1a4f63eaeceaafa5a18a40f25db7f781d6218
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-04 15:23:23 +0100

    feat: fontawesome setup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 
 <template>
   <h1>Hello</h1>
+  <!-- <font-awesome-icon icon="coffee" /> -->
 </template>
 
 <style scoped lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
-
+import App from './App.vue';
 // STYLE
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import './style/general.scss';
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App.vue';
-createApp(App).mount('#app');
+// Add all solid icons to the library
+library.add(fas);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
