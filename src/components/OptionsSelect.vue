7890a27ca0fc21020b71258d4e62381c40e849fe
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-04 17:04:06 +0100

    feat: currency selection logic

--- src/components/OptionsSelect.vue
+++ src/components/OptionsSelect.vue
@@ -1,25 +1,48 @@
-<script>
-    export default {
-        props: {
-            currencies: {
-                type: Array,
-                required: true
-            }
-        },
-        
-        methods: {
-            getCurrencyName(code) {
-                return currencyNames[code] || code; // Fallback to code if name not found
-            }
-        }
-    }
-</script>
-
 <template>
-    <option v-for="currency in currencies" :key="currency" :value="currency">
-        {{ currency }}
-    </option>
+    <select v-model="selectedCurrency" @change="updateCurrency">
+        <option v-for="(fullName, code) in currencies" :key="code" :value="code">
+            {{ code }} - {{ fullName }} 
+        </option>
+    </select>
 </template>
+
+<script>
+export default {
+    props: {
+        // Props from Main of all currencies
+        currencies: {
+            type: Object,
+            required: true
+        },
+        // Props from Main of v-model
+        modelValue: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            // The selectedCurrency it's the v-model value (the value it's the code)
+            selectedCurrency: this.modelValue // Initialize with the v-model value
+        };
+    },
+
+    methods: {
+        // Selecting a new value, emits the selected currency to parent v-model 
+        // -> the v-model updates the firstCurr or secondCurr variable that again will be passed to the component
+        updateCurrency() {
+            this.$emit('update:modelValue', this.selectedCurrency);
+        }
+    },
+
+    watch: {
+        // Watch for changes in the DOM when the props it's changed, the v-model syncs to the new value
+        modelValue(newValue) {
+            this.selectedCurrency = newValue;
+        }
+    },
+}
+</script>
 
 <style>
 </style>
